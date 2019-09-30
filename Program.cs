ce8dc764eb84446ac3abb862d05e91874e0f9102
vovve <vovve73@icloud.com>
2019-09-30 11:09:05 +0200

    Start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace excercises_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
